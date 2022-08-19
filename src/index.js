943f787e007a111abc6139398e6fdab28a177381
Josesan94 <josesantillan2@hotmai>
2022-08-19 00:20:22 -0300

    hoc implementing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+ import App from './App/index.js';
 import './index.css';
 
+// function App({saludo, nombre}) {
+//   return(
+//     <h1>{saludo}, {nombre}</h1>
+//   )
+// }
+// //HOC: dentro de su funcion devuelve otro componente
+// function HoCSaludo(WrappedComponent) {
+//   //funcion intermedia
+//   return function WrappedComponentHocSaludo(saludo) {
+    
+//     return function ComponenteDeVerdad(props) {
+//       return (
+//         <>
+//         <WrappedComponent {...props} saludo={saludo} />
+//         <p>Acompañando al wrappedComponent</p>
+//         </>
+//       )
+      
+//     }
+//   }
+// }
+// const AppWithHoc = HoCSaludo(App)('Buenas buenas')
+
+
+
 ReactDOM.render(
-  <App />,
+  <App saludo="buenas" nombre="Jose" />,
+  // <AppWithHoc nombre="jose"/>,
   document.getElementById('root')
 );
 
